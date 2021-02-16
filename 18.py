7053bcd216dec6d64363c954f0f6aae6b50a88a0
Zhou <lilaboc.cn@gmail.com>
2021-02-17 00:26:28 +0800

    18 and 67 done

---
+++ 18.py
@@ -0,0 +1,21 @@
+r = []
+with open('18.txt', 'r') as f:
+    for line in f.readlines():
+        r.append([int(i) for i in line.split()])
+
+
+print(r)
+for row_number in range(1, len(r)):
+    for item_number in range(len(r[row_number])):
+        if item_number == 0:
+            r[row_number][item_number] += r[row_number - 1][item_number]
+        elif item_number == len(r[row_number]) - 1:
+            r[row_number][item_number] += r[row_number - 1][item_number - 1]
+        else:
+            r[row_number][item_number] += max(r[row_number - 1][item_number - 1], r[row_number - 1][item_number])
+print(r)
+print(max(r[-1]))
+
+
+
+
